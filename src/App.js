cf9487d751d61aeb10c4e4aa0d6422261b847057
Micah Godbolt <micahgodbolt@gmail.com>
2017-05-09 10:09:36 -0700

    Step1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'office-ui-fabric-core/dist/css/fabric.min.css';
+import {
+  Fabric, 
+  TextField, 
+  PrimaryButton, 
+  Checkbox, 
+  ProgressIndicator
+} from "office-ui-fabric-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fabric className="App">
+
+        <TextField
+          label="Enter Todo"
+          ref={ (t) => this.field = t}
+          />
+          
+      </Fabric>
     );
   }
 }
